826a75716eaed4cd74901d667d2de30f997f23ee
Leo Chung <leo.chung97@gmail.com>
2023-11-30 12:56:45 -0500

    Initial Commit

---
+++ ConversionChallenge/ConversionChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionChallengeApp.swift
+//  ConversionChallenge
+//
+//  Created by Leo Chung on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ConversionChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
